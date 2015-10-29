49861a50d8b20d4beef7aba9bda8811de62d6603
Reto Gmür <reto@apache.org>
2015-10-29 22:08:26 +0100

    removed obsolete code

--- entry/src/META-INF/resources/js/entry.js
+++ entry/src/META-INF/resources/js/entry.js
@@ -9,19 +9,6 @@ $(function() {
 	new P3BackendConfigurator().initialize(configurator)
 			.then(
 					function() {
-						var host = window.location.hostname;
-						$(".dashboard-link").attr(
-								"href",
-								"http://" + host + ":8200/?platformURI="
-										+ window.location);
-						$(".host").each(function() {
-							$(this).text(host)
-						});
-						$("a").each(function() {
-							var origHref = $(this).attr("href");
-							var newHref = origHref.replace(/{host}/g, host);
-							$(this).attr("href", newHref);
-						});
 						$("#wait").hide();
 						$("#content").css('visibility', 'visible');
 						LD2h.expand();
