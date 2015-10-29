76c5ab0b1a971c6ca8e70d68f4fcce901a3f4f7a
Reto Gmür <reto@apache.org>
2015-10-29 11:49:07 +0100

    Looking for config script at fixed paths

--- entry/src/eu/fusepool/p3/entry/BackendConfigScript.scala
+++ entry/src/eu/fusepool/p3/entry/BackendConfigScript.scala
@@ -10,19 +10,36 @@ import org.apache.clerezza.rdf.ontologies._
 import eu.fusepool.p3.vocab.FP3
 import Preamble._
 import java.io.InputStreamReader
+import java.io.Reader
+import java.io.File
+import java.io.FileReader
 
 /**
  * This JAX-RS resource returns a js-script to configure the backend, i.e. to create the various registries.
  *
- * If an environment vraiable `P3-BACKEND-CONFIG-JS`is specified, the script at that path is used.
+ * If a script at ~/.fusepool-p3/backend-config.js exists it is used, otherwise
+ * if a script at /etc/fusepool-p3/backend-config.js exists it is used, otherwise
+ * a default script using services from sandbox.fusepool.info is used.
  */
 @Component(service = Array(classOf[Object]), property = Array("javax.ws.rs=true"))
 @Path("js/backend-config.js")
 class BackendconfigScript {
 
+  val homeDir = new File(System.getProperty("user.home"));
+  
+  def userFile = new File(homeDir, ".fusepool-p3/backend-config.js");
+  
+  def systemFile = new File("/etc/fusepool-p3/backend-config.js");
+  
+  
   @GET
-  def getScript = {
-    //TODO check env variable
-    new InputStreamReader(classOf[BackendconfigScript].getResourceAsStream("default-backend-config.js"))
+  def getScript : Reader = {
+    if (userFile.exists()) {
+      return new FileReader(userFile); 
+    }
+    if (systemFile.exists()) {
+      return new FileReader(systemFile); 
+    }
+    return new InputStreamReader(classOf[BackendconfigScript].getResourceAsStream("default-backend-config.js"))
   }
 }
